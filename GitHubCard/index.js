c46285079cd4ebc7ccb3f45d9c7653b28db54e25
Stuart Darsey <stuart.darsey@gmail.com>
2021-03-19 23:29:26 -0400

    my card appeared

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,21 +1,83 @@
+import axios from 'axios'
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-
+const stuGithub = axios.get('https://api.github.com/users/stu-wd')
+  .then(res => res.data) 
+  .catch(err => console.log(`error`))
+  .finally(() => console.log('finally'));
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
     data in order to use it to build your component function
-
-    Skip to STEP 3.
 */
+
+  // STEP 3: Create a function that accepts a single object as its only argument.
+  //   Using DOM methods and properties, create and return the following markup:
+
+function markupMaker (obj) {
+//create the elements
+  const card = document.createElement("div");
+  const img = document.createElement("img");
+  const cardInfo = document.createElement("div");
+  const githubName = document.createElement("h3");
+  const username = document.createElement("p");
+  const location = document.createElement("p");
+  const profile = document.createElement("p");
+  const profileLink = document.createElement("a");
+  const followers = document.createElement("p");
+  const following = document.createElement("p");
+  const bio = document.createElement("p");
+
+//classes
+  card.classList.add("card");
+  cardInfo.classList.add("card-info");
+  githubName.classList.add("name");
+  username.classList.add("username");
+
+//hierarchy
+  card.appendChild(img);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(githubName);
+  cardInfo.appendChild(username);
+  cardInfo.appendChild(location);
+  cardInfo.appendChild(profile);
+  profile.appendChild(profileLink);
+  cardInfo.appendChild(followers);
+  cardInfo.appendChild(following);
+  cardInfo.appendChild(bio);
+
+//text content
+  img.src = obj.avatar_url;
+  githubName.textContent = obj.name;
+  username.textContent = obj.login;
+  location.textContent = obj.location;
+  profileLink.textContent = obj.html_url;
+  followers.textContent = obj.followers;
+  following.textContent = obj.following;
+  bio.textContent = obj.bio;
+  
+  return card;
+}
 
 /*
   STEP 4: Pass the data received from Github into your function,
     and append the returned markup to the DOM as a child of .cards
 */
+
+//entry point
+const cards = document.querySelector(".cards");
+
+axios.get("https://api.github.com/users/stu-wd")
+  .then(res => {
+    let myCard = markupMaker(res.data);
+    cards.appendChild(myCard)
+  })
+  .catch(err => console.log("can't do that dude"))
+  .finally(() => console.log(`yah bud`));
 
 /*
   STEP 5: Now that you have your own card getting added to the DOM, either
@@ -26,35 +88,4 @@
 
     Using that array, iterate over it, requesting data for each user, creating a new card for each
     user, and adding that card to the DOM.
-*/
-
-const followersArray = [];
-
-/*
-  STEP 3: Create a function that accepts a single object as its only argument.
-    Using DOM methods and properties, create and return the following markup:
-
-    <div class="card">
-      <img src={image url of user} />
-      <div class="card-info">
-        <h3 class="name">{users name}</h3>
-        <p class="username">{users user name}</p>
-        <p>Location: {users location}</p>
-        <p>Profile:
-          <a href={address to users github page}>{address to users github page}</a>
-        </p>
-        <p>Followers: {users followers count}</p>
-        <p>Following: {users following count}</p>
-        <p>Bio: {users bio}</p>
-      </div>
-    </div>
-*/
-
-/*
-  List of LS Instructors Github username's:
-    tetondan
-    dustinmyers
-    justsml
-    luishrd
-    bigknell
 */
